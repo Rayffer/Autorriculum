42e5effdf347cb33a22af7933a363c395565e4f8
Rayffer <oscar.guillamon@gmail.com>
2021-03-02 13:46:35 +0100

    add argument handling and information. handle exceptions when trying to write to a file which is opened. add logging information

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from jinja2 import Template
 from docxtpl import DocxTemplate
 import json
+import argparse
 
-doc = DocxTemplate("curriculumTemplate.docx")
+parser = argparse.ArgumentParser("A program to generate customised curriculums from a template")
+parser.add_argument("templatePath", nargs='?', help="The path to the template to use (default: %(default)s)", 
+    type=str, default="curriculumTemplate.docx")
+parser.add_argument("informationFile", nargs='?', help="The path to the file containing the information to use in a json format (default: %(default)s)", 
+    type=str, default="information.json")
+parser.add_argument("resultPath", nargs='?', help="The path to write the result file in (default: %(default)s)", 
+    type=str, default="result.docx")
 
-with open("information.json", "r") as infile:
+args = parser.parse_args()
+
+print("Loading template from: " + args.templatePath)
+doc = DocxTemplate(args.templatePath)
+
+print("Loading and parsing JSON from: " + args.informationFile)
+with open(args.informationFile, "r", encoding='utf8') as infile:
     templateInfo = json.loads(infile.read())
+print("Printing information on the template")
 doc.render(templateInfo)
-doc.save("result.docx")
+
+try:
+    print("Attempting to save the result in the document: " + args.resultPath)
+    doc.save(args.resultPath)
+except PermissionError:
+    print("Cannot access the file, please close it and try again")
+    exit()
+
+print("Document saved successfully")
